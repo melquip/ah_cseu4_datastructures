9eb620da7239b7754ea304e7471e087f0c762b1c
Melquisedeque Pereira <melqui1@live.com.pt>
2020-04-23 17:59:19 +0100

    Initial queues and stacks commit

---
+++ ah_queues_and_stacks.py
@@ -0,0 +1,16 @@
+# Queue
+# First In First Out -> FIFO
+# A good example of a queue is any queue of consumers where the consumer that came first is served first
+
+
+# Stack
+# Last In First Out -> LIFO
+
+
+
+# The difference between queues and stacks is in removing.
+# In a queue, we remove the item the least recently added.
+# In a stack we remove the item the most recently added.
+
+# Implement Queue using Stacks
+# This method makes sure that oldest entered element is always at the top of stack 1, so that dequeue operation just pops from stack1. To put the oldest element at top of stack1, stack2 is used.
